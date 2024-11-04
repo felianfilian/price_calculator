cbade7ff415a0f3d80c779a924f9db422b179e7c
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-11-04 09:50:42 +0100

    price calc setup

--- main.go
+++ main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Print("price calculator")
+	var prices []float64 = []float64{10, 20, 30}
+	var taxRates []float64 = []float64{0, 0.7, 0.1, 0.2}
+
+	result := make(map[float64][]float64)
 }
