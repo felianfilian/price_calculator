566bf86a62e04de583d5bddb5dac429e439c7f0d
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-11-06 15:19:29 +0100

    calculator outputs

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("price calculator")
+	fmt.Println("price calculator")
 	var prices []float64 = []float64{10, 20, 30}
 	var taxRates []float64 = []float64{0, 0.7, 0.1, 0.2}
 
+
 	result := make(map[float64][]float64)
+
+	fmt.Println(prices)
+	fmt.Println(result)
+
+	for _, taxRate := range taxRates {
+		fmt.Println(taxRate)
+	} 
 }
